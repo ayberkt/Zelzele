272e94965421af56d3ee7a3567bfd9ee1dd98748
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-02-06 19:19:42 +0200

    Initial Commit

---
+++ Zelzele/ZELAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZELAppDelegate.h
+//  Zelzele
+//
+//  Created by Ayberk Tosun on 6/2/14.
+//  Copyright (c) 2014 Ayberk Tosun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZELAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Zelzele/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Zelzele
+//
+//  Created by Ayberk Tosun on 6/2/14.
+//  Copyright (c) 2014 Ayberk Tosun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZELAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZELAppDelegate class]));
+    }
+}
