51c457e1969c4f04e923225a3d8ca7458158369d
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-03-22 18:46:30 +0200

    Fix typo in gitignore

--- Zelzele/ZELEarthquakeTableViewController.m
+++ Zelzele/ZELEarthquakeTableViewController.m
@@ -11,7 +11,9 @@
 #import "AFNetworking/AFNetworking.h"
 #import "ZELEarthquakeViewCell.h"
 
-#define API_URL @"http://www.kimonolabs.com/api/de82dvke?apikey=59d4c680d16190d5be90519f464f2770"
+#include "API_KEY.h"
+
+#define API_URL @"http://www.kimonolabs.com/api/de82dvke?apikey="
 
 @interface ZELEarthquakeTableViewController ()
 
